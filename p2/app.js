ed1da217e07fcb678ee5a982ea2cd74ccd720705
Said <zitouni.sd@gmail.com>
2021-02-28 19:28:03 +0100

    project starter files

---
+++ p2/app.js
@@ -0,0 +1,13 @@
+/* Problem description: we need to create a page that contains 4 sections , when we hover over any section it get wider and we can see the full background image.
+
+user input : hovering
+desired output : background image git wider and full title of section appear.
+How to get the desired output : we probably need to adjust width of each section when hovering using js.
+
+Pseudo-code : 
+WHEN USER HOVER OVER SECTION
+    BACKGROUND IMAGE GET WIDER 
+    REST OF THE SECTION TITLE APPEAR IN TOP AND BOTTOM
+
+
+*/
