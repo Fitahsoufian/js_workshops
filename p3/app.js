c1df706bb5b73924fc9b92a29599e8d985a75dc9
hamza <terbouchi@gmail.com>
2021-03-03 11:24:40 +0100

    update js

--- p3/app.js
+++ p3/app.js
@@ -25,14 +25,13 @@ WHEN USER SEARCH
 
 
 
-const addForm = document.querySelector(".add");
+const form = document.querySelector(".add");
 let list = document.querySelector(".todos");
-const search = document.querySelector(".search > .form-control");
 
-const generateTemplate = (todo) => {
+function generateTemplate(toDo) {
     const item = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
-        <span>${todo}</span>
+        <span>${toDo}</span>
         <svg class="delete" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -41,13 +40,13 @@ const generateTemplate = (todo) => {
     list.innerHTML += item
 };
 
-addForm.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const todo = addForm.add.value.trim();
+    const todo = form.add.value.trim();
     if (todo.length) {
         generateTemplate(todo)
-        addForm.reset()
+        form.reset()
     }
 
 });
@@ -58,18 +57,3 @@ list.addEventListener('click', e => {
         e.target.parentElement.remove()
     }
 })
-
-// // filter todos
-// const filterTodo = term => {
-//     Array.from(list.children)
-//         .filter(todo => !todo.textContent.toLowerCase().includes(term))
-//         .forEach(todo => todo.classList.add('filtered'));
-
-//     Array.from(list.children)
-//         .filter(todo => todo.textContent.toLowerCase().includes(term))
-//         .forEach(todo => todo.classList.remove('filtered'));
-// }
-// search.addEventListener('keyup', () => {
-//     const term = search.value.trim().toLowerCase()
-//     filterTodo(term)
-// })
